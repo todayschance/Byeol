799f83f1f94c1568097305968d2aa9ead8e52aa8
Ahn Nyeong <compiztab@daum.net>
2014-02-12 22:15:09 +0900

    Rails installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Byeol::Application.config.secret_key_base = '279e8f54c3b4497024223abf04f1a32b1e9ca354b79ed7bcd7d88440a2ffffd574020185d435af59011ec9acaa3e6540c5b745fca51b52e4c2dbbb0e2c9d2e71'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Byeol::Application.config.session_store :cookie_store, key: '_Byeol_session'
