9d15eabcdeeea47010cb2f6b8cde48d1a981c772
Ahn Nyeong <compiztab@daum.net>
2014-02-14 19:15:40 +0900

    Fixed missing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
 	def full_title page_title
 		base_title = "별바다"
 		if page_title.empty?
-			"#{base_title}: 읽고, 나눕시"
+			"#{base_title}: 읽고, 나눕시다."
 		else
 			"#{page_title} | #{base_title}"
 		end
