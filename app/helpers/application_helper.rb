0ca1231fd02057f6bd9f5624870bf5938463aae2
Ahn Nyeong <compiztab@daum.net>
2014-02-14 17:24:08 +0900

    The views are edited

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	# Returns the full title of head of html.
+	def full_title page_title
+		base_title = "별바다"
+		if page_title.empty?
+			"#{base_title}: 서평 커뮤니티"
+		else
+			"#{page_title} | #{base_title}"
+		end
+		
+	end
 end
