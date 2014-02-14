a869bb315e524974541b2e776f3fb80dac365f76
Ahn Nyeong <compiztab@daum.net>
2014-02-14 18:03:32 +0900

    rails d controller StaticPage home help

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Byeol::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
+
+  root 'controller#static_pages'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
