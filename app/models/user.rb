d25cb1cda5d7886301d7a136820a16c1d29465bf
Ahn Nyeong <compiztab@daum.net>
2014-02-16 16:26:33 +0900

    basic user model with encrypt password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,11 @@ class User < ActiveRecord::Base
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	
-	validates :name, presence: true, length: { maximum: 10 }
-	validates :email, presence: true, uniqueness: { case_sensitive: false }, format: {with: VALID_EMAIL_REGEX}
+	validates :name, presence: true,
+									 length: { maximum: 10 }
+	validates :email, presence: true,
+										uniqueness: { case_sensitive: false },
+										format: {with: VALID_EMAIL_REGEX}
 	validates :password, length: { minimum: 6 }
 
 end
