b1b9c3b75c2d5e61c86933678af2e2283ffa7c74
Ahn Nyeong <compiztab@daum.net>
2014-02-21 22:59:58 +0900

    mailers destroied

--- app/mailers/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserMailer < ActionMailer::Base
-  default from: "web_master@byeolbada.herokuapp.com"
-
-  def welcome_email user
-  	@user = user
-  	@url = 'http://byeolbada.herokuapp.com/'
-  	mail to: @user.email, subject: "별바다에 오신 것을 환영합니다!"
-  end
-end
