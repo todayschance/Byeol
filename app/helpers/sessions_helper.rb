66e5f647a81000a71bb42728a3eecb3d49896b3f
Ahn Nyeong <compiztab@daum.net>
2014-03-01 13:06:33 +0900

    updating 작업 및 레이아웃 개선. erb에서 haml로
    will_paginate 지켜봐야

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,6 +15,11 @@ module SessionsHelper
 		@current_user ||= User.find_by(remember_token: remember_token)
 	end
 
+	def current_user?(user)
+		user == current_user
+		
+	end
+
 	def signed_in?
 		!current_user.nil?
 	end
@@ -24,4 +29,37 @@ module SessionsHelper
 		cookies.delete(:remember_token)
 		self.current_user = nil
 	end
+
+	def store_location
+		session[:return_to] = request.url if request.get?
+	end
+
+	def redirect_back_or(default)
+		redirect_to(session[:return_to] || default)
+		session.delete(:return_to)
+	end
+
+	# layouts
+	def field_for(target, icon, title, symbol, type, placeholder="")
+		output = '<div class="field">'
+		output << target.label(symbol, title)
+		output << '<div class="ui left icon input">'
+		output << icon_tag(icon)
+		output << target.text_field(symbol, type: type, placeholder: placeholder)
+		output << '</div></div>'
+		return output.html_safe
+	end
+
+	def described_field_for(target, icon, title, symbol, type, placeholder="", describe)
+		output = '<div class="grid-r"><div class="field grid-1-2">'
+		output << target.label(symbol, title)
+		output << '<div class="ui left icon input">'
+		output << icon_tag(icon)
+		output << target.text_field(symbol, type: type, placeholder: placeholder)
+		output << '</div></div><div class="mobile_text_right grid-1-2"><small>'
+		output << describe
+		output << "</small></div></div>"
+		return output.html_safe
+	end
+
 end
