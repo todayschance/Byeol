3dec362fede74fa3efd2d999f5b4add597efceed
Ahn Nyeong <compiztab@daum.net>
2014-03-12 20:32:15 +0900

    rails installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Byeol::Application.config.secret_key_base = '598ad02ba11d3271a17208df1dfec6c02b001dce945c7b95a4b1b39fe7e4673d8172bdcaf924811abe1c340d1c2163e606cb8f27f23f63ba1848325852926ac4'
